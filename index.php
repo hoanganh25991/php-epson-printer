deb147eeb1bd1f0df4f872721da301aa43344a83
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-07-23 10:15:17 +0700

    custom for json

--- index.php
+++ index.php
@@ -1,44 +1,52 @@
 <?php
-/* Call this file 'hello-world.php' */
 require __DIR__ . '/vendor/autoload.php';
 require_once './util.php';
-use Mike42\Escpos\PrintConnectors\FilePrintConnector;
 use Mike42\Escpos\Printer;
 use Mike42\Escpos\PrintConnectors\NetworkPrintConnector;
 
-hoiEcho("Hello");
+// Allow cors
+header("Access-Control-Allow-Origin: *");
+// header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Max-Age: 1000");
+header("Access-Control-Allow-Headers: access-control-allow-origin,content-type");
+// header("Access-Control-Allow-Methods: PUT, POST, GET, OPTIONS, DELETE");
+//header("Content-Type: application/json");
 
 try{
-	handleRequest();
+    handleRequest();
 }catch(\Exception $e){
-	hoiEcho($e->getMessage());
-	return;
+    hoiEcho($e->getMessage());
+    return;
 }
 
 function handleRequest(){
-	if(isPost()){
-		$text = $_POST['text'];
-		hoiPrint($text);
-	}else{
-		echo "See you";
-	}
+    hoiEcho("Printer Server");
+    if(isPost()){
+        $requestBody = file_get_contents('php://input');
+        $req = json_decode($requestBody, true);
+        hoiPrint($req['text']);
+    }else{
+        echo "See you";
+    }
 }
 
 function hoiPrint($text){
-	
-
-	// $connector = new FilePrintConnector("php://stdout");
-	// $connector = new FilePrintConnector("/dev/usb/lp0");
-	$connector = new NetworkPrintConnector("192.168.1.121", 9100);
-	$printer = new Printer($connector);
-	try {
-	  // ... Print stuff
-	  $printer->text($text);
-	  $printer->cut();
-	  $printer->close();
-	} catch(\Exception $e) {
-	  echo $e->getMessage();
-	}
+    // $connector = new FilePrintConnector("php://stdout");
+    // $connector = new FilePrintConnector("/dev/usb/lp0");
+    hoiEcho("Set up connection to printer");
+    $connector = new NetworkPrintConnector("192.168.1.121", 9100);
+    $printer = new Printer($connector);
+    try{
+        // ... Print stuff
+        hoiEcho("Printing...");
+        $printer->text($text);
+        hoiEcho("Cutting...");
+        $printer->cut();
+        hoiEcho("Finishing...");
+        $printer->close();
+    }catch(\Exception $e){
+        echo $e->getMessage();
+    }
 }
 
 
